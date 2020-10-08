43aa907d2c5d8da2b6123a28a667718e380c1888
343dev <343dev@users.noreply.github.com>
2020-10-08 18:05:15 +0500

    Add showing diffs by categories

--- tasks/diff.js
+++ tasks/diff.js
@@ -1,15 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
+const diffTotal = require('../lib/diff-total');
+
 module.exports = function diff(left, right) {
-  const leftSnapshotPath = path.resolve(left);
-  const rightSnapshotPath = path.resolve(right);
+  const leftPath = path.resolve(left);
+  const rightPath = path.resolve(right);
 
-  const leftSnapshotBuffer = fs.readFileSync(leftSnapshotPath);
-  const rightSnapshotBuffer = fs.readFileSync(rightSnapshotPath);
+  const leftBuffer = fs.readFileSync(leftPath);
+  const rightBuffer = fs.readFileSync(rightPath);
 
-  if (leftSnapshotBuffer.equals(rightSnapshotBuffer)) {
+  if (leftBuffer.equals(rightBuffer)) {
     console.log('Snapshots are equal');
     process.exit(0);
   }
+
+  const leftSnapshot = JSON.parse(leftBuffer.toString());
+  const rightSnapshot = JSON.parse(rightBuffer.toString());
+
+  // Total diff
+  console.log('Overall differences by category:');
+  console.log(diffTotal({
+    left: leftSnapshot.total,
+    right: rightSnapshot.total,
+    leftCaption: path.parse(leftPath).name,
+    rightCaption: path.parse(rightPath).name,
+  }));
 };
